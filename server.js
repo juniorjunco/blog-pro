ae8ea8ef96c7deeeb95a25556e30cf495372a399
juniorjunco <juniorjunco@gmail.com>
2024-08-14 22:55:36 -0500

    yap

--- server.js
+++ server.js
@@ -429,6 +429,136 @@ app.get('/news', async (req, res) => {
 });
 
 
+// Apartado noticias en english 
+
+const englishNewsSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const EnglishNews = mongoose.model('EnglishNews', englishNewsSchema);
+
+
+app.post('/news-en', authenticateToken, upload.single('image'), async (req, res) => {
+  try {
+    const { title, description } = req.body;
+    let imageUrl = null;
+
+    if (req.file) {
+      const bufferStream = new Readable();
+      bufferStream.push(req.file.buffer);
+      bufferStream.push(null);
+
+      imageUrl = await new Promise((resolve, reject) => {
+        const uploadStream = cloudinary.uploader.upload_stream(
+          { resource_type: 'auto' },
+          (error, result) => {
+            if (error) {
+              reject('Error uploading image to Cloudinary');
+            }
+            resolve(result.secure_url);
+          }
+        );
+        bufferStream.pipe(uploadStream);
+      });
+    }
+
+    const news = new EnglishNews({
+      title,
+      description,
+      image: imageUrl,
+      date: new Date(),
+    });
+    await news.save();
+    res.status(201).json({ success: true, news });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+});
+
+app.put('/news-en/:id', authenticateToken, upload.single('image'), async (req, res) => {
+  try {
+    const newsId = req.params.id;
+    const { title, description } = req.body;
+    let imageUrl = null;
+
+    if (req.file) {
+      const bufferStream = new Readable();
+      bufferStream.push(req.file.buffer);
+      bufferStream.push(null);
+
+      imageUrl = await new Promise((resolve, reject) => {
+        const uploadStream = cloudinary.uploader.upload_stream(
+          { resource_type: 'auto' },
+          (error, result) => {
+            if (error) {
+              reject('Error uploading image to Cloudinary');
+            }
+            resolve(result.secure_url);
+          }
+        );
+        bufferStream.pipe(uploadStream);
+      });
+    }
+
+    const news = await EnglishNews.findById(newsId);
+
+    if (!news) {
+      return res.status(404).json({ success: false, message: 'News not found' });
+    }
+
+    news.title = title;
+    news.description = description;
+    if (imageUrl) {
+      news.image = imageUrl;
+    }
+    await news.save();
+    res.status(200).json({ success: true, news });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+});
+
+app.delete('/news-en/:id', authenticateToken, async (req, res) => {
+  try {
+    const newsId = req.params.id;
+    const news = await EnglishNews.findById(newsId);
+
+    if (!news) {
+      return res.status(404).json({ success: false, message: 'News not found' });
+    }
+
+    await EnglishNews.findByIdAndDelete(newsId);
+    res.status(200).json({ success: true, message: 'News deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+});
+
+app.get('/news-en', async (req, res) => {
+  try {
+    const news = await EnglishNews.find();
+    res.json(news);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+
 // Iniciar servidor
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
